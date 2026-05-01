d4382fdf7aa58ae47b10387d1c374080f5856cda
Clark-kent-crypto <clarkkent65yu@gmail.com>
2026-05-01 19:46:20 +0530

    Add files via upload

---
+++ RtoD.py
@@ -0,0 +1,8 @@
+def rtod(radius):
+    import math as mt
+    degree=radius*(180/mt.pi);
+    return degree;
+def dtor(degree):
+    import math as mt
+    radius=degree*(mt.pi/180);
+    return radius;
